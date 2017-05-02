ca84a191746540aa05f50c6d0d8ce318df63d095
Brian Rush and Corey Redding <brian+corey@>
2017-05-02 16:58:47 -0700

    add javascript each function to checkboxes in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  $("post-epicodus").submit(function(event) {
+  $("#post-epicodus").submit(function(event) {
     event.preventDefault();
+    $("#fortune-answer").show();
+    $("input:checkbox[name=easy-concepts]:checked").each(function() {
+      var easy = $(this).val();
+      $("#fortune-answer").append(easy);
+    });
+    $("input:checkbox[name=hard-concepts]:checked").each(function() {
+      var hard = $(this).val();
+      $("#fortune-answer").append(hard);
+    });
+    $("#post-epicodus").hide();
   });
 });
